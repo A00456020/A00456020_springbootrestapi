9be8b87d8431ec13bc65f9e56ce6702adab0e9b0
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-04 21:54:11 -0400

    Added getHotel function to return hotel object in json

--- src/main/java/com/assignment/springbootapp/controller/HotelController.java
+++ src/main/java/com/assignment/springbootapp/controller/HotelController.java
@@ -1,7 +1,9 @@
 package com.assignment.springbootapp.controller;
 
 
+import com.assignment.springbootapp.entity.Hotel;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +11,10 @@ public class HotelController {
     @GetMapping("/hello")
     public String sayHi(){
         return "Hello!";
+    }
+
+    @GetMapping("/hotel")
+    public Hotel getHotels(@RequestParam(value="id", defaultValue = "1") int id){
+        return new Hotel(id, "Ahmedabad Inn", "", 280, 26);
     }
 }
