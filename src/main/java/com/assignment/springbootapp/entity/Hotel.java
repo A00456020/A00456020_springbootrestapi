a549b433f8b599cdf7be98af42dd00364380d8ad
Parth Tarak Vaidya <Parth.Tarak.Vaidya@smu.ca>
2022-03-04 21:44:52 -0400

    Added Hotel constructor and getters and setters for all the attributes

--- src/main/java/com/assignment/springbootapp/entity/Hotel.java
+++ src/main/java/com/assignment/springbootapp/entity/Hotel.java
@@ -2,6 +2,11 @@ package com.assignment.springbootapp.entity;
 
 public class Hotel {
 
+    public int id;
+    public String name;
+    public String website_url;
+    public int total_number_of_rooms;
+
     public Hotel(int id, String name, String website_url, int total_number_of_rooms, int available_rooms) {
         this.id = id;
         this.name = name;
@@ -10,9 +15,45 @@ public class Hotel {
         this.available_rooms = available_rooms;
     }
 
-    public int id;
-    public String name;
-    public String website_url;
-    public int total_number_of_rooms;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWebsite_url() {
+        return website_url;
+    }
+
+    public void setWebsite_url(String website_url) {
+        this.website_url = website_url;
+    }
+
+    public int getTotal_number_of_rooms() {
+        return total_number_of_rooms;
+    }
+
+    public void setTotal_number_of_rooms(int total_number_of_rooms) {
+        this.total_number_of_rooms = total_number_of_rooms;
+    }
+
+    public int getAvailable_rooms() {
+        return available_rooms;
+    }
+
+    public void setAvailable_rooms(int available_rooms) {
+        this.available_rooms = available_rooms;
+    }
+
     public int available_rooms;
 }
